de33590053fefc3494e385a6fd42a9d014359397
Wellington Moreira <wellington.msjr10@gmail.com>
2025-05-20 07:52:32 -0300

    Finish Section 9: Debug, trhow, try catch

--- exemplo/api/operations.js
+++ exemplo/api/operations.js
@@ -5,9 +5,12 @@ const {
     divide } = require("../services/functions");
 
 function apiCalculate (req, res) {
-    const {num1, num2, signal} = req.body
-    let result = 0
+    try{
 
+        const {num1, num2, signal} = req.body
+        let result = 0
+        
+    
     switch (signal){
 
         case '+':
@@ -29,28 +32,50 @@ function apiCalculate (req, res) {
         
         default:
             res.send({error:'Invalid option'})
+        }
+    }  catch (e){
+        res.status(500).send({msg: e.message})
     }
 }
 
 function apiSum (req, res) {
-    const {num1, num2} = req.body
-    const result = sum (num1, num2)
-    res.send({result})
+    try{
+
+        const {num1, num2} = req.body
+        const result = sum (num1, num2)
+        res.status(200).send({result})
+    } catch (e){
+        res.status(500).send({msg: e.message})
+    }
 }
 function apiSubtract (req, res) {
-    const {num1, num2} = req.body
-    const result = subtract (num1, num2)
-    res.send({result})
+    try{
+        const {num1, num2} = req.body
+        const result = subtract (num1, num2)
+        res.send({result})
+    }  catch (e){
+        res.status(500).send({msg: e.message})
+    }
 }
 function apiDivide (req, res) {
-    const {num1, num2} = req.body
-    const result = divide (num1, num2)
-    res.send({result})
+    try{
+
+        const {num1, num2} = req.body
+        const result = divide (num1, num2)
+        res.send({result})
+    } catch (e){
+        res.status(500).send({msg: e.message})
+    }
 }
 function apiMultiplies (req, res) {
-    const {num1, num2} = req.body
-    const result = multiplies (num1, num2)
-    res.send({result})
+    try{
+
+        const {num1, num2} = req.body
+        const result = multiplies (num1, num2)
+        res.send({result})
+    } catch (e){
+        res.status(500).send({msg: e.message})
+    }
 }
 module.exports = {
     apiCalculate,
